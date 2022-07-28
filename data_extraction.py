6f772648e68cf5fa39c6536d00eb5a5d980408d3
Matthew Do <domattgmt7@gmail.com>
2022-07-28 19:49:56 +0700

    sanitized inputs

--- data_extraction.py
+++ data_extraction.py
@@ -33,22 +33,26 @@ movies = re.compile(r'<h3 class="editorial-film-listitem__title">(.*?)</h3>', re
 if __name__ == "__main__":
     cwd = os.getcwd()
 
-    csv_in = pd.DataFrame(columns=['person', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'])
+    csv_in = pd.DataFrame(columns=['person', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'])
 
     if os.path.exists(os.path.join(cwd, 'offline_links')):
         for html in os.listdir(os.path.join(cwd, 'offline_links')):
-            with open(os.path.join(cwd, 'offline_links', html), 'r', encoding='utf-8') as f:
-                html = f.read()
-                soup = BeautifulSoup(html, 'html.parser')
+            with open(os.path.join(cwd, 'offline_links', '93.html'), 'r', encoding='utf-8') as f:
+                content = f.read()
+                soup = BeautifulSoup(content, 'html.parser')
+                test = soup.find('article').get_attribute_list('data-article-title')
+                csv_in['person'] = str(test).replace('’s Top 10', '')[2:-2]
                 for index in soup.find_all('p', attrs={'class': 'count'}):
                     i = re.search(r'(10|[1-9])', index.text, re.MULTILINE)
                     if i is not None:
-                        csv_in[i.group(0)] = "sherry shit"
-                    # print(index.next_sibling.next_sibling)
+                        print(i.group(0))
+                        titles = BeautifulSoup(str(index.next_sibling.next_sibling), 'html.parser')
+                        print(index.next_sibling.next_sibling)
+                        print(titles.find_all('h3', attrs={'class': 'editorial-film-listitem__title'}))
 
-                # print(re.search(title, html).group(1))
-                # print(re.findall(director, html))
-                # print(re.findall(movies, html))
+                break
+            break
+
         csv_in.to_csv('data.csv', sep='^', index=False, mode='w', encoding='utf-8')
     else:
         exit()
